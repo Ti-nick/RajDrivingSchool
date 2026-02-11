53d715ee6b0bc5fed3a0a4daae452cf5f629d89d
nickchoi-monash <nick.choi.it@gmail.com>
2026-02-11 13:48:24 +1100

    Build for GitHub Pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../Home.vue";
 import Contact from "../Contact.vue";
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     { path: "/", component: Home },
     { path: "/contact", component: Contact }
